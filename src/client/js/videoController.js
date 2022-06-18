abd0f943fc8596f404eed01045c4ddd601db506a
rsuubinn <rsuubinn@gmail.com>
2022-06-18 23:15:23 +0900

    Video Controller

--- src/client/js/videoController.js
+++ src/client/js/videoController.js
@@ -1,7 +1,53 @@
+const video = document.querySelector("video");
 const playBtn = document.getElementById("playBtn");
+const playBtnIcon = playBtn.querySelector("i");
+const muteBtn = document.getElementById("muteBtn");
+const muteBtnIcon = muteBtn.querySelector("i");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
 
 const handlePlayBtnClick = () => {
-  alert("hi");
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
+};
+
+const handleMuteBtnClick = () => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtnIcon.classList = video.muted
+    ? "fas fa-volume-up"
+    : "fas fa-volume-mute";
+};
+
+const formatTime = (seconds) => {
+  return new Date(seconds * 1000).toISOString().substring(14, 19);
+};
+
+const handleLoadedMetaData = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
+};
+
+const handleCurrentTime = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+  console.log(event.target.value);
+  video.currentTime = event.target.value;
 };
 
 playBtn.addEventListener("click", handlePlayBtnClick);
+muteBtn.addEventListener("click", handleMuteBtnClick);
+video.addEventListener("loadedmetadata", handleLoadedMetaData);
+video.addEventListener("timeupdate", handleCurrentTime);
+timeline.addEventListener("input", handleTimelineChange);
